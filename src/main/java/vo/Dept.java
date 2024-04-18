5a22bd3c8d3d2e73fd78e9583479fec6d12f66db
leeDragonHun <9leeyh5@gmail.com>
2024-04-18 10:15:54 +0900

    dept, emp테이블 vo 생성

---
+++ src/main/java/vo/Dept.java
@@ -0,0 +1,9 @@
+package vo;
+
+// 부여타입.
+// VO(Value Object), DTO(Data Transfer Object), Domain(Dept 라는 테이블과 동일하게 만들 것이라서 (==속성값의 범위)
+public class Dept {
+	public int deptNo;
+	public String dname;
+	public String loc;
+}
